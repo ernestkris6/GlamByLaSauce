673bd8ccd63d2649efe15eed2f185ed31b189bb0
ERNEST CHRIS <ernestchris6@gmail.com>
2025-10-03 20:14:29 +0100

    creating pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,3 @@ import React from 'react'
 
 //https://www.figma.com/design/a5Qy7QsBDuwweCjBpUbwhZ/Makeup-website--Community-?node-id=0-1&p=f&t=f73OolI0QXpV02oL-0
 
-export default function App() {
-  return (
-    <div>
-      <h1 className='text-2xl text-center mt-8 font-bold'>Hello World ! ! !</h1>
-    </div>
-  )
-}
