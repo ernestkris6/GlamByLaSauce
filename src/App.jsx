ecfae48e20c81b3a838bc1d1676fe0f274381e03
ERNEST CHRIS <ernestchris6@gmail.com>
2025-10-03 21:40:13 +0100

    defining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import Works from './Pages/Works'
+import Services from './Pages/Services'
+import Error from './Pages/Error'
 
 //https://www.figma.com/design/a5Qy7QsBDuwweCjBpUbwhZ/Makeup-website--Community-?node-id=0-1&p=f&t=f73OolI0QXpV02oL-0
 
+
+export default function App() {
+  return ( 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='works' element={<Works />} />
+          <Route path='services' element={<Services />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
