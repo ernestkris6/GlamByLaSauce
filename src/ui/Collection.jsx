93933e2a4dcf9d3e7aae9792d549b35556d1a94d
ERNEST CHRIS <ernestchris6@gmail.com>
2025-10-10 19:57:51 +0100

    adding collection bg

--- src/ui/Collection.jsx
+++ src/ui/Collection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-// import bg from '../assets/bg.jpg'
+import bg from '../assets/bg.jpg'
 
 export default function Collection() {
   return (
    <div>
-        <section className='relative bg-[url(./bg.jpg)] mt-16 md:mt-14 bg-cover bg-no-repeat pb-10 w-full h-[50vh] md:h-[65vh] mb-14'>
+        <section 
+        style={{backgroundImage: `url(${bg})`}}
+        className={`relative mt-16 md:mt-14 bg-cover bg-no-repeat pb-10 w-full h-[50vh] md:h-[65vh] mb-14`}>
             <div className='absolute justify-center items-center top-12 bottom-12 sm:top-18 sm:bottom-18 bg-[#fff]'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae libero officia nemo necessitatibus. Alias, est obcaecati. Magni quod aliquam facere similique minus labore, quam totam maiores tempore, quibusdam nihil odit.</p>
             </div>
@@ -13,7 +15,7 @@ export default function Collection() {
   )
 }
 
-
+//url(${bg})
 //w-full absolute bottom-0 right-0 max-w-md
 //bg-cover bg-center bg-no-repeat min-h-screen flex items-center justify-center text-white
 
