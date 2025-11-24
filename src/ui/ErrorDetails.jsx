d082730cc6d478ff172ffa806a9c81dc06852ce2
ERNEST CHRIS <ernestchris6@gmail.com>
2025-11-24 21:17:53 +0100

    using semantic tags

--- src/ui/ErrorDetails.jsx
+++ src/ui/ErrorDetails.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function ErrorDetails() {
   return (
     <div className='my-12'>
-        <section className='border-2 border-amber-700 rounded-sm max-w-[368px] flex flex-col items-center justify-center md:hover:bg-amber-50 mx-auto py-12'>
+        <section className='flex flex-col border-1 border-amber-700 rounded-sm mx-auto max-w-[368px] items-center justify-center md:hover:bg-amber-50  py-12'>
             <h1 className='font-bold pb-6'>Error: Page not Found😢</h1>
             <p className='font-medium text-[15px] pb-4'>Kindly click the button below to begin your glam😍</p>
             <Link to='/'>
